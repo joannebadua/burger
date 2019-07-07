49276d5d67a4aef79d29736cfcbcb5ba4df1aefd
Joanne Badua <joannebadua@gmail.com>
2019-07-07 11:33:48 -0700

    add object for SQL statement functions

--- config/orm.js
+++ config/orm.js
@@ -7,6 +7,40 @@ function printQuestionMarks(num) {
     }
     return arr.toStrin();
 }
+//helper function that converts object key/value pairs to SQL syntax
 function objToSql(ob) {
-    var arr 
+    var arr = [];
+    //loop through the keys and push the key/value as a string into array
+    for (var key in ob){
+        var value = ob[key];
+        //check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if(typeofvalue === "string" && value.indexOf("")>+0) {
+                arr.push(key + "=" + value);
+            }
+        }
+        return arr.toString();
+    }
+}
+//object for all our sql statement functions
+var orm = {
+    all:function(tableInput, cb){
+        var queryString = "SELECT * FROM" + tableInput + ";";
+        connection.query(queryString, function(err, result){
+            if(err){
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    create: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+        queryString += "(";
+        queryString += cols.toString();
+        queryString += ")";
+        queryString += "VALUES (";
+        queryString += printQuestionMARKS (vals.length);
+        queryString += ")";
+        console.log(queryString);
+    }
 }
