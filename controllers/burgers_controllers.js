958684ac05749739658487d17ed973a60f68f4a4
Joanne Badua <joannebadua@gmail.com>
2019-07-07 12:08:04 -0700

    create routes and post request for a submit form

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,3 +1,22 @@
 var express = require("express");
 var router = express.router();
 //import the model (burgers.js)
+var burgers = require("..models/burgers.js");
+//create all our routes and set up logic within those routes where required
+router.get("/", function(req, res) {
+burgers.all(function(data){
+    var hbsObject = {
+        burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+});
+});
+//POST Request for a form submit 
+router.post("/burgers", function(req, res){
+    burgers.create(["name", "devoured"], [req.body.name, req.body.devoured], 
+    function(result){
+    //send back the ID of the new quote
+    res.json({id : result.insertId});
+    });
+});
