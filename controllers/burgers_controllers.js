f01b641a9bfa8ed06da1f5dc57486528992bc112
Joanne Badua <joannebadua@gmail.com>
2019-07-07 12:17:13 -0700

    include put request to update burgers being devoured and export routes for server.js to use

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -20,3 +20,22 @@ router.post("/burgers", function(req, res){
     res.json({id : result.insertId});
     });
 });
+router.put("/burgers/:id", function (req, res) {
+    var condition ="id = " + req.params.id;
+    console.log("condition", condition);
+    burgers.update(
+    {
+        devoured: req.body.devoured
+    },
+    condition,
+    function(result) {
+        if(result.changedRows === 0) {
+            //if no rows were changed, then the ID must not exist so 404
+            return res.status(404).end();
+        }
+        res.status(200).end();
+        }
+        );
+    });
+    //export routes for server.js to use
+    module.exports=router;
